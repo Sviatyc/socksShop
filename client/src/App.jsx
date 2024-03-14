75abb2cacb5e123ec42653ae676b42455199fc70
Sviatoslav Nakonechnyi <dielok4@gmail.com>
2024-03-14 02:04:12 +0200

    add tailwind to my client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -32,17 +32,19 @@ function App() {
 
   return (
     <>
-      <h1>СПИСОК АДМІНІВ</h1>
-      <div style={{display: 'flex', flexDirection: 'column'}}>
+      <h1 className="ml-2 mt-4 border-2 border-pink-500 w-[150px]">СПИСОК АДМІНІВ</h1>
+      <div className="flex flex-col">
         {users.map(item => {
-          return <div key={item.id}>{item.login}</div>
+          return <div key={item.id} className="ml-2 text-purple-500">{item.login}</div>
         })}
       </div>
-      <div style={{display: 'flex', flexDirection: 'column', width: '300px', gap: '5px'}}>
+      <div className="flex flex-col w-[200px] gap-[5px] ml-2">
         <h2>Добавити адміна</h2>
-        <input type="text" value={login} onChange={(e)=>setLogin(e.target.value)}/> LOGIN
-        <input type="text" value={password} onChange={(e)=>setPassword(e.target.value)}/> PASSWORD
-        <button onClick={create_admin}>add admin</button>
+        <p>LOGIN</p>
+        <input type="text" value={login} onChange={(e)=>setLogin(e.target.value)} className="border-2 border-black rounded-xl px-2"/> 
+        <p>PASSWORD</p>
+        <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)} className="border-2 border-black rounded-xl px-2"/>
+        <button onClick={create_admin} className="border-2 border-black rounded-[4px] mt-2">add admin</button>
       </div>
     </>
   )
